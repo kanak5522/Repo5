7f3958fe0b27fda6117988442ec76bffef42ace5
kanak5522 <kanakchandel552000@gmail.com>
2024-09-04 00:35:46 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "t1ff_state" {
-  bucket = "kanak-bucketnme"
-  acl    = "private"
-}
 
-resource "aws_dynamodb_table" "t1fflock" {
-  name           = "lock"
-  hash_key        = "LockID"
-  read_capacity   = 1
-  write_capacity  = 1
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 
 resource "aws_instance" "newinta" {
     ami = "ami-04e49d62cf88738f1"
